06f4642b43741a5fc26df490e145ba8ac1b5d7a8
elsyelins <sihombing.elisabet55@gmail.com>
2021-10-12 17:46:21 +0700

    java-code-challenge-1

---
+++ JavaBasic.java
@@ -0,0 +1,24 @@
+public class JavaBasic {
+    public static void main (String[] args){
+        String firstname;
+        firstname =" Elizabeth";
+        String lastname = " Yelin Melani";
+        String address = " Jl. Bajak II H Gg Coklat Medan";
+        String phonenumber = " 081375296982";
+        String hobby = " Membaca dan Memasak";
+        String favfood = " Roti, Kue, dan Mie";
+        String favdrink = " Matcha latte";
+        String favwriter = " Ika Natassa, Tsana, AliaZalea";
+        int age = 20;
+            
+        System.out.println("Name :" + firstname + lastname);
+        System.out.println ("Age :" + age);
+        System.out.println ("Address :" + address);
+        System.out.println ("Phone Number :" + phonenumber);
+        System.out.println ("Hobby :" + hobby);
+        System.out.println ("Favorite Food :" + favfood);
+        System.out.println ("Favorite Drink :" + favdrink);
+        System.out.println ("Favorite Writer :" + favwriter);
+
+    }
+}
